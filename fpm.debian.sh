af516d4343e79478e7e7eb6b15d77bca5265bc3e
britannic <github@empirecreekcircle.com>
2018-01-10 15:00:20 -0800

    Version 1.2.1
    Debian package release.

--- fpm.debian.sh
+++ fpm.debian.sh
@@ -1,40 +1,80 @@
 #!/usr/bin/env bash
 
+export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
+export COPYFILE_DISABLE=true
+drop="$HOME/Dropbox/EdgeOS"
 arch='all'
 dep='dnsutils'
 desc='Adds EdgeOS DDNS configuration templates for Cloudflare and RFC2136'
 dir='/tmp/'
 dist='deb'
+exc1=\**/.DS_Store
+exc2=\**/sbin
 host='dev1'
 lic='http://www.apache.org/licenses/LICENSE-2.0'
+mntr="${pkg}@helmrock.com"
 pkg='edgeos-rfc2136-ddns'
 post='./post-install.sh'
 pre='./pre-install.sh'
+prov='Adds EdgeOS DDNS configuration templates for Cloudflare and RFC2136'
 sdir='payload/rfc2136.dynamic.dns/'
 src1='usr/'
 src2='opt/'
-upgr='vyatta-cfg-system'
 url='https://github.com/britannic/EdgeMax-RFC2136-DDNS'
+vend='Helm Rock Consulting'
 ver=$(cat VERSION)
-who='Helm Rock Consulting'
 
-fpm \
+bundle ()
+{
+  cwd=$(pwd)
+  local dir='./payload/rfc2136.dynamic.dns/opt/vyatta/sbin'
+  local target='vyatta-dynamic-dns.pl'
+  local templt='decompress_pre-install'
+
+  cat "${templt}" > "${pre}"
+  cd "${dir}"
+
+  tar -zc \
+  --exclude='._*' \
+  --exclude='.svn' \
+  --exclude='.DS_Store' \
+  --exclude='*.bak' \
+  --exclude='*~' \
+  ./* >> "${cwd}/${pre}"
+  cd "${cwd}"
+}
+
+make_debian ()
+{
+  fpm \
     -f \
     --after-remove "${post}" \
     --before-install "${pre}" \
     --deb-pre-depends "${dep}" \
     --description "${desc}" \
     --license "${lic}" \
-    --replaces "${upgr}" \
+    --provides "${prov}" \
     --url "${url}" \
-    --vendor "${who}" \
+    --vendor "${vend}" \
     --verbose \
     -a "${arch}" \
     -C "${sdir}" \
-    -m "${who}" \
+    -m "${mntr}" \
     -n "${pkg}"  \
     -t "${dist}" \
     -v "${ver}"  \
-    -x \**/.DS_Store \
-    -s dir "${src1}" "${src2}" \
-    && scp "${pkg}_${ver}_${arch}.${dist}" "${host}:${dir}"
+    -x "${exc1}" \
+    -x "${exc2}" \
+    -s dir "${src1}" "${src2}"
+}
+
+update_dropbox () {
+  [[ -d "${drop}/" ]] && install -m 0755 "${pkg}_${ver}_${arch}.${dist}" "${drop}/"
+}
+
+upload () {
+  scp "${pkg}_${ver}_${arch}.${dist}" "${host}:${dir}"
+}
+
+# Build it!
+bundle && make_debian && update_dropbox && upload
--- post-install.sh
+++ post-install.sh
@@ -2,7 +2,9 @@
 
 target='/opt/vyatta/sbin/vyatta-dynamic-dns.pl'
 
-if [[ -e "${target}.bkp" ]]; then
-  echo "${DPKG_MAINTSCRIPT_PACKAGE} - restoring ${target}..."
-  mv "${target}.bkp" "${target}"
+if [[ -e ${target}.dpkg ]]; then
+  if ! cmp -s ${target}.dpkg ${target}; then
+    echo "${DPKG_MAINTSCRIPT_PACKAGE} - restoring ${target}..."
+    mv "${target}.dpkg" "${target}"
+  fi
 fi
