46bbd9b73db18a98d17b3135b9480d3d6e422df2
britannic <github@empirecreekcircle.com>
2018-01-09 21:13:03 -0800

    Version 1.2.1 Debian Package Release

--- fpm.debian.sh
+++ fpm.debian.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
 
-arch="all"
-dir="/tmp/"
-dist="deb"
-host="dev1"
-pkg="edgeos-rfc2136"
+arch='all'
+dep='dnsutils'
+desc='Adds EdgeOS DDNS templates for Cloudflare and RFC2136'
+dir='/tmp/'
+dist='deb'
+host='dev1'
+lic='http://www.apache.org/licenses/LICENSE-2.0'
+pkg='edgeos-rfc2136-ddns'
+upgr='vyatta-cfg-system'
+sdir='payload/rfc2136.dynamic.dns/'
+src1='usr/'
+src2='opt/'
+url='https://github.com/britannic/EdgeMax-RFC2136-DDNS'
 ver=$(cat VERSION)
+who='Helm Rock Consulting'
 
 fpm -f \
-    --description "Adds EdgeOS DDNS templates for Cloudflare and RFC2136" \
-    --license "http://www.apache.org/licenses/LICENSE-2.0" \
-    --replaces vyatta-cfg-system \
+    --description "${desc}" \
+    --license "${lic}" \
+    --replaces "${upgr}" \
+    --vendor "${who}" \
     --verbose \
-    -a ${arch} \
-    -C payload/rfc2136.dynamic.dns/ \
-    -m "Helm Rock Consulting" \
-    -n ${pkg}  \
-    --deb-pre-depends dnsutils \
-    -t ${dist} \
-    --url "https://github.com/britannic/EdgeMax-RFC2136-DDNS"
-    -v ${ver}  \
+    --url "${url}" \
+    --deb-pre-depends "${dep}" \
+    -a "${arch}" \
+    -C "${sdir}" \
+    -m "${who}" \
+    -n "${pkg}"  \
+    -t "${dist}" \
+    -v "${ver}"  \
     -x \**/.DS_Store \
-    -s dir usr/ opt/ \
-    && scp "${pkg}_${ver}_${arch}.${dist}" ${host}:${dir}
+    -s dir "${src1}" "${src2}" \
+    && scp "${pkg}_${ver}_${arch}.${dist}" "${host}:${dir}"
